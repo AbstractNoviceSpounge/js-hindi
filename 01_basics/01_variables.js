58d1b21023747dab057271399af081c8781b4dc8
Varun Tyagi <tyagi.varun2017@gmail.com>
2025-09-03 06:19:21 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "varun@gmail.com"
+var accountPassword = "12345"
+accountCity = "gzb"
+let accountState
+
+// accountId = 2 //changes not allowed
+console.log(accountId);
+accountEmail = "1@gmail.com"
+accountPassword = "787878"
+accountCity = "Delhi"
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+
+let name = "Varun"
+let age = 32
+let isLoggedIn = false
+
+//number => range is 2^53
+//bigint
+//string=>""
+//boolean
+//null
+//undefined
+//symbol => unique
+// object
+
+
+console.log(typeof "Varun");
+console.log(typeof null);
+
