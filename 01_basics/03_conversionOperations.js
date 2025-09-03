8a121f2dce629eab322d4d3be42e8f374d5996c0
Varun Tyagi <tyagi.varun2017@gmail.com>
2025-09-03 07:00:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = "33"
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof(valueInNumber));
+ 
+
+ // "33" -> 33
+ // "77hs" -> NaN
+ // "True" -> 1
+ // "False" -> 0
+ // Null -> 0
+ // "Varun" -> NaN
+ // Undefined -> NaN 
+
+ let isLoggedIn = 1
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+ 
