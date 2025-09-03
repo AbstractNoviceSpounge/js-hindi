3be7e748fcef89b36bb7d1df0997ea91017aeb98
Varun Tyagi <tyagi.varun2017@gmail.com>
2025-09-03 11:41:07 +0000

    datatypes-summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,32 @@
+// Primitive datatypes -> call by value -> do not change the actual value
+        // String, Number, boolean, null, undefined, symbol, bigint
+
+
+const id = Symbol('12')
+const id2 = Symbol('12')
+
+console.log(id);
+console.log(id2);
+
+console.log(id === id2);
+
+const bigNumber = 87647774889765465796234567698n
+
+console.log(bigNumber);
+
+
+
+// non primitive -> call by reference -> 
+        // arrays, objects, functions
+
+const heroes = ["Shaktiman", "Naagraj", "Captain Dhruv"]
+
+let myObj = {
+        name: "Varun",
+        age: 33,
+}
+
+const myFunction = function(){
+        console.log("Hello worlds");
+        
+}
