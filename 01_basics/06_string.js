161685171f337b69bacfb67b6443f88ef22c5c34
Varun Tyagi <tyagi.varun2017@gmail.com>
2025-09-03 14:49:56 +0000

    strings

---
+++ 01_basics/06_string.js
@@ -0,0 +1,19 @@
+const name = "Varun"
+const repoCount = 10
+
+console.log(name + " " + repoCount + " value");    // old fashioned
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('varunvt')  //string declare karne ka ek aur tarika
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
+
+
+
+
