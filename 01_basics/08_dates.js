9b9776083b67b6b735f52972c2d244fe2b6fff12
Varun Tyagi <tyagi.varun2017@gmail.com>
2025-09-04 09:43:44 +0000

    dates

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+
+
+let myTimeStamp = Date.now()
